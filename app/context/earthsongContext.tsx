c5e2072c0c209ce7752d767e1e937bb81bbcb507
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-04-01 09:31:34 +0300

    update replacement context

--- app/context/earthsongContext.tsx
+++ app/context/earthsongContext.tsx
@@ -1,9 +1,37 @@
-import { createContext } from 'react';
+'use client';
+import {
+  createContext,
+  type Dispatch,
+  type FC,
+  type ReactNode,
+  type SetStateAction,
+  useState,
+} from 'react';
 
 interface EarthsongContextType {
   themeValue: 'light' | 'dark';
+  setThemeValue: Dispatch<SetStateAction<'light' | 'dark'>>;
 }
 
 export const earthsongContext = createContext<EarthsongContextType>({
-  themeValue: 'dark',
+  themeValue: 'light',
+  setThemeValue: () => {},
 });
+
+interface Props {
+  children: ReactNode;
+  initial?: 'light' | 'dark';
+}
+
+export const EarthsongContextProvider: FC<Props> = ({
+  children,
+  initial = 'light',
+}) => {
+  const [themeValue, setThemeValue] = useState(initial);
+
+  return (
+    <earthsongContext.Provider value={{ themeValue, setThemeValue }}>
+      {children}
+    </earthsongContext.Provider>
+  );
+};
