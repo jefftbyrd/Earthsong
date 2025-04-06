366bf9644a0c1f7fae35754cd6c964e424433880
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-04-06 10:20:42 +0300

    start applying tailwind to portal

--- app/components/Portal.js
+++ app/components/Portal.js
@@ -37,7 +37,7 @@ export default function Portal() {
         />
       )}
 
-      <div className={styles.multiController}>
+      <div className="">
         {isMobile ? (
           <SoundControllerMobile
             soundsColor={soundsColor}
