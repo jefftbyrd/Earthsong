09bc4316f3f950f53b89a72c479714454b3ab9b3
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-04-10 11:58:12 +0300

    add new Login component

---
+++ app/components/panels/Login.jsx
@@ -0,0 +1,92 @@
+import { useRouter } from 'next/navigation';
+import { useContext, useEffect, useState } from 'react';
+import { journeyContext } from '../../context/journeyContext';
+import ErrorMessage from '../../ErrorMessage';
+import { Button, ButtonText } from '../ui';
+
+export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
+  const [registerOpen, setRegisterOpen] = useState(false);
+  // const { togglePanel } = useContext(journeyContext);
+
+  const router = useRouter();
+
+  async function handleLogin(event) {
+    event.preventDefault();
+
+    const response = await fetch('api/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+
+    const data = await response.json();
+
+    if ('errors' in data) {
+      setErrors(data.errors);
+      return;
+    }
+
+    router.refresh();
+  }
+
+  return (
+    <>
+      <p className="text-xl">Please sign in.</p>
+
+      <form onSubmit={handleLogin}>
+        <label>
+          Name
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            // id="username"
+            // type="text"
+            placeholder="identify yourself"
+            // autoFocus={true}
+            value={username}
+            onChange={(event) => setUsername(event.currentTarget.value)}
+          />
+        </label>
+
+        <label>
+          Password
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            // id="password"
+            // type="password"
+            // placeholder="******************"
+            // autoFocus={true}
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.currentTarget.value)}
+          />
+        </label>
+
+        <Button type="submit">sign in</Button>
+
+        {errors.map((error) => (
+          <div className="error" key={`error-${error.message}`}>
+            <ErrorMessage>{error.message}</ErrorMessage>
+          </div>
+        ))}
+      </form>
+
+      <p>
+        If you don't have an account, you should{' '}
+        <ButtonText
+          className=""
+          onClick={() => {
+            setRegisterOpen(!registerOpen);
+          }}
+        >
+          register
+        </ButtonText>
+        . Registered users can save their journeys and return to them later.
+      </p>
+    </>
+  );
+}
