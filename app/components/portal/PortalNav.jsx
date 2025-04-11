a3befd8ad3efc0a9d0599eba5cbf4ca8d91dbde9
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-04-11 13:23:18 +0300

    hide map menu item on landing and map pages

--- app/components/portal/PortalNav.jsx
+++ app/components/portal/PortalNav.jsx
@@ -6,7 +6,7 @@ import EarthsongIcons from '../EarthsongIcons';
 import { panels } from './panelConfig';
 
 export default function PortalNav({ isLoggedIn }) {
-  const { togglePanel, setPanelId, setReset, setPhase } =
+  const { togglePanel, setPanelId, setReset, setPhase, phase } =
     useContext(journeyContext);
   // const { user } = useContext(userContext);
   const [resetDone, setResetDone] = useState(false);
@@ -26,20 +26,23 @@ export default function PortalNav({ isLoggedIn }) {
 
   return (
     <footer className="h-10 border-t-1 bg-black w-full grid grid-cols-3 uppercase">
-      <button
-        key="map"
-        className="uppercase text-center flex items-center justify-center gap-2 text-lg"
-        onClick={() => {
-          setReset(true);
-          setTimeout(() => {
-            setReset(false);
-            setResetDone(true);
-          }, 0);
-        }}
-      >
-        <EarthsongIcons className="h-6 w-6" iconNumber={5} />
-        <span>Map</span>
-      </button>
+      {phase === 'portal' || phase === 'portalRecall' ? (
+        <button
+          key="map"
+          className="uppercase text-center flex items-center justify-center gap-2 text-lg"
+          onClick={() => {
+            setReset(true);
+            setTimeout(() => {
+              setReset(false);
+              setResetDone(true);
+            }, 0);
+          }}
+        >
+          <EarthsongIcons className="h-6 w-6" iconNumber={5} />
+          <span>Map</span>
+        </button>
+      ) : null}
+
       {filteredPanels.map(([id, { label, icon }]) => (
         <button
           key={`panel-${id}`}
