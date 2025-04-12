4b439467a1ab93025770fae39f2c9a6791b11df9
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-04-12 15:24:27 +0300

    add playsound using useSound

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,28 @@
 // tailwind.config.js
 
-const config = {
-  theme: {
-    extend: {
-      fontFamily: {
-        basteleur: ['var(--font-basteleur)'],
-      },
+// const config = {
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         basteleur: ['var(--font-basteleur)'],
+//       },
+//     },
+//   },
+// };
+
+// export default config;
+
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  './pages/**/*.{js,ts,jsx,tsx}',
+  './components/**/*.{js,ts,jsx,tsx}',
+  './app/**/*.{js,ts,jsx,tsx}',
+];
+export const theme = {
+  extend: {
+    fontFamily: {
+      basteleur: ['var(--font-basteleur)'],
     },
   },
 };
-
-export default config;
+export const plugins = [];
