60161d60bf1b43b4a14a40ee843482d6439e89e0
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-04-14 18:59:32 +0300

    add soundNumber

--- app/components/portal/SoundNumber.jsx
+++ app/components/portal/SoundNumber.jsx
@@ -1,28 +1,9 @@
-import { motion } from 'motion/react';
-
 export default function SoundNumber({ soundNumber = 1 }) {
   // Define all sound patterns
   const aegean = ['𐄇', '𐄈', '𐄉', '𐄊', '𐄋'];
 
-  // Only apply animation when isPlaying is true
-  // const animationProps = isPlaying
-  //   ? {
-  //       animate: {
-  //         rotate: [0, 360],
-  //       },
-  //       transition: {
-  //         ease: 'linear',
-  //         duration: 1.5,
-  //         repeat: Infinity,
-  //       },
-  //     }
-  //   : {};
-
   return (
-    <div
-      className="w-14 h-14 grid place-items-center origin-center text-black relative"
-      // {...animationProps}
-    >
+    <div className="w-6 h-10 lg:w-14 lg:h-14 grid place-items-center origin-center text-black relative">
       <span
         className={`font-(family-name:--font-noto) text-7xl xl:text-9xl leading-none absolute ${
           soundNumber === 4 || soundNumber === 5 ? '-translate-y-2' : ''
