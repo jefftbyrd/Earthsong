9e57bb40558f41042e89b9c21d5656fe52c3f1b4
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-04-15 15:55:14 +0300

    add setPin dependency to map

--- app/components/Map.jsx
+++ app/components/Map.jsx
@@ -71,7 +71,7 @@ export default function Map() {
     return () => {
       mapRef.current.remove();
     };
-  }, []); // Empty dependency array - create map only once
+  }, [setPin]); // Empty dependency array - create map only once
 
   return (
     <>
