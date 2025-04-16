f60a7d20c8c5642f2ba6b1266b6f66ea0c6a4a29
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-04-16 10:43:15 +0300

    add PWA support

--- app/layout.jsx
+++ app/layout.jsx
@@ -50,6 +50,18 @@ export default async function RootLayout({ children }) {
     <html lang="en">
       <head>
         <title>Earthsong</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <meta name="theme-color" content="#000000" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
 
         {/* <meta
           name="viewport"
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+import NextPWA from 'next-pwa';
+
+const withPWA = NextPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
+
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA(nextConfig);
