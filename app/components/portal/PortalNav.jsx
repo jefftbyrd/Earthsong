ab5b340b2f3194dff64ab43892a7c7353277421d
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-04-17 12:16:04 +0300

    update portal nav

--- app/components/portal/PortalNav.jsx
+++ app/components/portal/PortalNav.jsx
@@ -1,4 +1,5 @@
 'use client';
+import { AnimatePresence, motion } from 'motion/react';
 import React, { useContext } from 'react';
 import { journeyContext } from '../../context/journeyContext';
 import { useSoundPlayer } from '../../context/soundPlayerContext';
@@ -8,14 +9,7 @@ import { panels } from './panelConfig';
 export default function PortalNav({ isLoggedIn }) {
   const { togglePanel, setPanelId, phase, triggerReset2, panelId, panelOpen } =
     useContext(journeyContext);
-  // const [resetDone, setResetDone] = useState(false);
   const { setActivateTarget } = useSoundPlayer();
-
-  // useEffect(() => {
-  //   if (resetDone) {
-  //     setPhase('map');
-  //   }
-  // }, [resetDone, setPhase]);
 
   // Prevent event propagation to canvas
   const handleInteraction = (e) => {
@@ -41,26 +35,37 @@ export default function PortalNav({ isLoggedIn }) {
         pointerEvents: 'auto',
       }}
     >
-      {phase === 'portal' || phase === 'portalRecall' ? (
-        <button
-          key="map"
-          className="uppercase text-center flex items-center justify-center gap-0  hover:bg-[#0033FF] transition-all duration-100 ease-in-out"
-          onClick={async (e) => {
-            e.stopPropagation(); // Extra safeguard
-            setActivateTarget(false);
-            togglePanel();
-            try {
-              await triggerReset2({ nextPhase: 'returnToMap' }); // Pass the required object
-              console.log('Reset triggered successfully');
-            } catch (error) {
-              console.error('Error triggering reset:', error);
-            }
-          }}
-        >
-          <EarthsongIcons className="h-6 w-6 mr-1" iconNumber={5} />
-          <span>Map</span>
-        </button>
-      ) : null}
+      <AnimatePresence>
+        {phase === 'portal' || phase === 'portalRecall' ? (
+          <motion.button
+            animate={{
+              opacity: [0, 1],
+              transition: { duration: 1 },
+            }}
+            exit={{
+              opacity: 0,
+              transition: { duration: 1 },
+            }}
+            key="map"
+            className="uppercase text-center flex items-center justify-center gap-0  hover:bg-[#0033FF] transition-all duration-100 ease-in-out"
+            onClick={async (e) => {
+              e.stopPropagation(); // Extra safeguard
+              setActivateTarget(false);
+              togglePanel();
+              try {
+                await triggerReset2({ nextPhase: 'returnToMap' }); // Pass the required object
+                console.log('Reset triggered successfully');
+              } catch (error) {
+                console.error('Error triggering reset:', error);
+              }
+            }}
+          >
+            <EarthsongIcons className="h-6 w-6 mr-1" iconNumber={5} />
+            <span>Map</span>
+          </motion.button>
+        ) : null}
+      </AnimatePresence>
+
       <div className="col-span-2 col-start-2 grid grid-cols-2 gap-0">
         {filteredPanels.map(([id, { label, icon, iconClassName }]) => (
           <button
@@ -71,19 +76,13 @@ export default function PortalNav({ isLoggedIn }) {
               setActivateTarget(false);
               if (panelOpen && panelId === id) {
                 togglePanel(); // This will close the panel and clear panelId
-              }
-              // Otherwise, set this panel as active
-              else {
+              } else {
                 setPanelId(id);
                 // The useEffect will handle opening the panel if needed
               }
             }}
           >
-            <EarthsongIcons
-              // className="h-6 w-6"
-              iconNumber={icon}
-              className={iconClassName}
-            />
+            <EarthsongIcons iconNumber={icon} className={iconClassName} />
             <span>{label}</span>
           </button>
         ))}
