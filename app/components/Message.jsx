cdb75cdfd4f0ca1827420fe032525798748068fd
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-04-19 13:20:01 +0300

    update message fade animations

--- app/components/Message.jsx
+++ app/components/Message.jsx
@@ -6,6 +6,7 @@ export default function Message({ text, isVisible, animationProps }) {
   return (
     <motion.h1
       className="message uppercase"
+      initial={{ opacity: 0 }}
       animate={{
         opacity: [0, 1, 0],
         ...animationProps, // Allow custom animations
