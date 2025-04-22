141e382037b49d455a801c4c9e50bda374d58f7b
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-04-22 10:09:49 +0300

    update guide panel layout

--- app/components/panels/GuidePanel.jsx
+++ app/components/panels/GuidePanel.jsx
@@ -27,80 +27,82 @@ export default function GuidePanel() {
 
   return (
     <PanelWrap panel="Guide" bg="#5381C4">
-      <button
-        className="relative w-4/5 lg:w-1/2 aspect-video overflow-hidden border-2 border-black/30 mt-3"
-        onClick={(e) => {
-          e.stopPropagation();
-          if (panelOpen && panelId === 'Navigate') {
-            togglePanel();
-          } else {
-            setPanelId('Navigate');
-          }
-        }}
-      >
-        <img
-          src="/guide-navigate.webp"
-          className="absolute inset-0 object-cover opacity-50 w-full h-full"
-          alt="Navigation"
-          width={1600}
-          height={900}
-        />
-        <div className="absolute inset-0 flex items-center justify-center">
-          <h1 className="text-4xl text-white z-10 text-shadow-md lg:text-7xl">
-            Navigation
-          </h1>
-        </div>
-      </button>
+      <div className="w-full flex flex-col items-center justify-center gap-1">
+        <button
+          className="relative w-4/5 lg:w-1/2 aspect-24/7 overflow-hidden border-1 border-black/50 mt-3"
+          onClick={(e) => {
+            e.stopPropagation();
+            if (panelOpen && panelId === 'Navigate') {
+              togglePanel();
+            } else {
+              setPanelId('Navigate');
+            }
+          }}
+        >
+          <img
+            src="/guide-navigate.webp"
+            className="absolute inset-0 object-cover opacity-50 w-full h-full"
+            alt="Navigation"
+            width={1600}
+            height={900}
+          />
+          <div className="absolute inset-0 flex items-center justify-center">
+            <h1 className="text-4xl text-white z-10 text-shadow-lg/30 lg:text-7xl">
+              Navigation
+            </h1>
+          </div>
+        </button>
 
-      <button
-        className="relative w-4/5 lg:w-1/2 aspect-video overflow-hidden"
-        onClick={(e) => {
-          e.stopPropagation();
-          if (panelOpen && panelId === 'Portal') {
-            togglePanel();
-          } else {
-            setPanelId('Portal');
-          }
-        }}
-      >
-        <Image
-          src="/guide-portal.webp"
-          className="absolute inset-0 object-cover opacity-50 w-full h-full rounded-md"
-          alt="Portal"
-          width={1600}
-          height={900}
-        />
-        <div className="absolute inset-0 flex items-center justify-center">
-          <h1 className="text-4xl text-white z-10 text-shadow-md lg:text-7xl">
-            Sound Portal
-          </h1>
-        </div>
-      </button>
+        <button
+          className="relative w-4/5 lg:w-1/2 aspect-24/7 overflow-hidden border-1 border-black/50"
+          onClick={(e) => {
+            e.stopPropagation();
+            if (panelOpen && panelId === 'Portal') {
+              togglePanel();
+            } else {
+              setPanelId('Portal');
+            }
+          }}
+        >
+          <Image
+            src="/guide-portal.webp"
+            className="absolute inset-0 object-cover opacity-50 w-full h-full"
+            alt="Portal"
+            width={1600}
+            height={900}
+          />
+          <div className="absolute inset-0 flex items-center justify-center">
+            <h1 className="text-4xl text-white z-10 text-shadow-lg/30 lg:text-7xl">
+              Sound Portal
+            </h1>
+          </div>
+        </button>
 
-      <button
-        className="relative w-4/5 lg:w-1/2 aspect-video overflow-hidden"
-        onClick={(e) => {
-          e.stopPropagation();
-          if (panelOpen && panelId === 'About') {
-            togglePanel();
-          } else {
-            setPanelId('About');
-          }
-        }}
-      >
-        <Image
-          src="/guide-about.webp"
-          className="absolute inset-0 object-cover opacity-50 w-full h-full rounded-md"
-          alt="About"
-          width={1600}
-          height={900}
-        />
-        <div className="absolute inset-0 flex items-center justify-center">
-          <h1 className="text-4xl text-white z-10 text-shadow-md lg:text-7xl">
-            What is it?
-          </h1>
-        </div>
-      </button>
+        <button
+          className="relative w-4/5 lg:w-1/2 aspect-24/7 overflow-hidden border-1 border-black/50"
+          onClick={(e) => {
+            e.stopPropagation();
+            if (panelOpen && panelId === 'About') {
+              togglePanel();
+            } else {
+              setPanelId('About');
+            }
+          }}
+        >
+          <Image
+            src="/guide-about.png"
+            className="absolute inset-0 object-cover opacity-60 w-full h-full"
+            alt="About"
+            width={1600}
+            height={900}
+          />
+          <div className="absolute inset-0 flex items-center justify-center">
+            <h1 className="text-4xl text-white z-10 text-shadow-lg/30 lg:text-7xl">
+              What is it?
+            </h1>
+          </div>
+        </button>
+      </div>
     </PanelWrap>
   );
 }
