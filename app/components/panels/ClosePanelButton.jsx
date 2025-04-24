05d9089a2e9de66c9a3658941ad482373b8cc2a4
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-04-24 12:42:30 +0300

    delete unused

--- app/components/panels/ClosePanelButton.jsx
+++ app/components/panels/ClosePanelButton.jsx
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import { journeyContext } from '../../context/journeyContext';
 
 export default function ClosePanelButton({ panel }) {
-  const { setPanelId, panelOpen, togglePanel, panelId, setPanelOpen } =
+  const { setPanelId, panelOpen, togglePanel, panelId } =
     useContext(journeyContext);
 
   return (
@@ -10,8 +10,6 @@ export default function ClosePanelButton({ panel }) {
       className="h-7 w-7 lg:h-10 lg:w-10 text-black/30 lg:text-white/70 fixed right-1 lg:right-5 flex items-center justify-center font-[--font-noto] text-5xl lg:text-8xl pb-1 lg:pb-2 cursor-pointer"
       onClick={(e) => {
         e.stopPropagation();
-        // console.log('Setting panelOpen to false');
-        // setPanelOpen(false);
         // If this exact panel is already open, just toggle it closed
         if (panelOpen && panelId === panel) {
           togglePanel(); // This will close the panel and clear panelId
