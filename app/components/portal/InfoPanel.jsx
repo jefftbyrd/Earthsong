e61cdae628b12f980b214a0d8067968203de80f1
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-04-28 10:24:14 +0300

    remove unused from info panel

--- app/components/portal/InfoPanel.jsx
+++ app/components/portal/InfoPanel.jsx
@@ -4,21 +4,17 @@ import { journeyContext } from '../../context/journeyContext';
 import { useDynamicHeight } from '../../hooks/useDynamicHeight';
 import ClosePanelButton from '../panels/ClosePanelButton';
 
-export default function InfoPanel({ sound, index, color }) {
+export default function InfoPanel({ sound, color }) {
   const minutes = Math.floor(sound?.duration / 60);
   const seconds = Math.floor(sound?.duration % 60)
     .toString()
     .padStart(2, '0');
   const geotagSplit = sound?.geotag.split(' ');
   const location = `${Number(geotagSplit[0]).toFixed(4)}, ${Number(geotagSplit[1]).toFixed(4)}`;
-  const { setPanelId, panelOpen, togglePanel, panelId } =
-    useContext(journeyContext);
+  const { panelOpen, togglePanel } = useContext(journeyContext);
   const { ref } = useDynamicHeight();
-  // const aegean = ['𐄇', '𐄈', '𐄉', '𐄊', '𐄋'];
 
   const cleanDescription = sound.description.replace(/<[^>]*>/g, '');
-  // const adjustColor = color.replace('1)', '0.5)').replace(/\s/g, '');
-  // const adjustColor = color.replace(/\s/g, '');
 
   useEffect(() => {
     // Create handler function for document-level keyboard events
@@ -42,9 +38,6 @@ export default function InfoPanel({ sound, index, color }) {
       ref={ref}
       className=" text-black box-border overflow-auto mt-0.5 z-40 relative lg:m-20 lg:border-black/50 lg:border-x-6 lg:border-t-6"
       style={{ backgroundColor: color }}
-      // role="dialog" // Indicate this is a dialog
-      // aria-modal="true" // Indicate this is a modal dialog
-      // aria-labelledby="info-panel-title" // Reference to the title element
     >
       <ClosePanelButton panel={sound.id} />
       <div className="p-5 lg:p-9 text-black ">
