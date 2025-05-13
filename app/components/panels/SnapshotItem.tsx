af44e8b455f5be26aba82628b3c97c81c81ab992
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-05-13 11:24:15 +0300

    update snapshot item

--- app/components/panels/SnapshotItem.tsx
+++ app/components/panels/SnapshotItem.tsx
@@ -60,8 +60,6 @@ export default function SnapshotItem({
         }}
       >
         <div className="flex flex-row gap-0 justify-between w-full border-black/30 border-l-2 border-b-2 border-solid py-3 pl-4 pr-2">
-          {/* <div className="inline-block w-0 h-0 border-solid border-t-[120px] border-r-0 border-l-[15px] border-b-0 border-l-[#000000]/50 border-r-transparent border-t-transparent border-b-transparent"></div> */}
-
           <div className="flex flex-col gap-1 ml-0 my-1 pr-5 w-full  ">
             <h3 className="font-bold text-xl mb-2 text-left tracking-wide">
               {snapshot.title}
@@ -88,7 +86,7 @@ export default function SnapshotItem({
               ) : null}
             </div>
             <button
-              className="  text-md uppercase font-basteleur tracking-widest text-left text-white bg-black/30 hover:bg-black/70 active:bg-white/50 active:text-black w-fit p-3 border-2 border-[rgb(255,0,89)]"
+              className="  text-md uppercase font-basteleur tracking-widest text-left text-white bg-black/40 hover:bg-black/70 active:bg-white/50 active:text-black w-fit p-3 outline-1 outline-offset-2 outline-black/30"
               onClick={async () => {
                 try {
                   // Close panel and reset targets
@@ -125,14 +123,12 @@ export default function SnapshotItem({
           </div>
 
           <div className="flex ml-auto">
-            {/* <div className="inline-block w-0 h-0 border-solid border-t-[90px] border-r-0 border-l-[15px] border-b-0 border-l-[#000000]/50 border-r-transparent border-t-transparent border-b-transparent"></div> */}
             <button
               onClick={handleDelete}
-              className="hover:text-white text-black/70 lg:text-sm [writing-mode:vertical-rl] [text-orientation:upright] tracking-[0em] text-xs border-black/30  border-l-1 pl-1.5 h-auto w-auto"
+              className="hover:text-white text-black/70 lg:text-sm [writing-mode:vertical-rl] [text-orientation:upright] tracking-[0.6em] lg:tracking-[0.2em] text-xs border-black/30  border-l-4 pl-1.5 h-auto w-auto border-double"
             >
               remove
             </button>
-            {/* <div className="inline-block w-0 h-0 border-solid rotate-180 border-t-[90px] border-r-0 border-l-[15px] border-b-0 border-l-[#000000]/50 border-r-transparent border-t-transparent border-b-transparent"></div> */}
           </div>
         </div>
       </motion.div>
